dcbc474de333fba03e17fd94a8c9a1b0d6c7354e
LNCFSV1H\DamianBergia <damian.bergia@na21.co.jp>
2022-05-27 12:02:13 +0900

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('#back').click(function(){
+        window.location.href='../index.html'; 
+});
+
+$('#back2').click(function(){
+    alert("データーは保存しました"); 
+    window.location.href='../index.html'; 
+});
+
+});
